cde0114310102c067b1030f9f4fd0bc72cf1be7f
PakM4k <biagio.padula@gmail.com>
2021-03-31 02:08:12 +0200

    added rtcDS3231 control

--- src/definitions.h
+++ src/definitions.h
@@ -1,14 +1,35 @@
 #ifndef DEFINITIONS_H_
 #define DEFINITIONS_H_
 
-#define ms  1000UL
-#define s   1000UL * ms
-#define m   60UL *s 
+#define MS_TO_US    1000UL
+#define S_TO_MS     1000UL
+#define M_TO_MS     60UL * S_TO_MS
 
-#define SLEEP_TIME 3600UL*s
+#define SLEEP_TIME  60UL*M_TO_MS*MS_TO_US
 
 // Nome Rete Wi-Fi e relativa password
-#define SSID "#andràtuttobere"
-#define  PASSWORD "ciucciomio"
+#define SSID            "#andràtuttobere"
+#define PASSWORD       "ciucciomio"
+
+// Nome di questo nodo
+#define NOME_NODO       "Nodo1"
+
+// Endpoint email
+#define ENDPOINT_EMAIL  "http://www.padulab.it/nodoremoto/send_email_event.php"
+
+// Endpoint API
+#define ENDPOINT_API    "http://www.padulab.it/nodoremoto/api.php"
+
+//PIN DEFINITION
+#define PWM_SENSOR_PIN        16
+#define READ_SENSOR_PIN       A0
+
+#define PWM_SENSOR_FREQ       50000
+#define PWM_SENSOR_CHANNEL    2
+#define PWM_SENSOR_RESOLUTION 8
+
+//EEPROM
+#define EEPROM_SIZE     1
+#define CLOCK_SET_ADDR  0
 
 #endif //DEFINITIONS_H_
--- src/main.cpp
+++ src/main.cpp
@@ -1,34 +1,18 @@
 
-/*
-    Progetto 2 Nodo Singolo Remoto
-
-    Codice del progetto
-
-    Lorenzo Neri
-*/
-
 // Includiamo le librerie
 
 #include <WiFiUdp.h>
 #include <WiFi.h>
 #include <Time.h>
-#include "NTPClient.h"
+#include "NTP.h"
 #include <HTTPClient.h>
+#include "RTClib.h"
+#include <EEPROM.h>
 
 #include "definitions.h"
 #include "gen_func.h"
 
-
-
-// Nome di questo nodo
-
-String nomeNodo = "Nodo1";
-
-// Endpoint email
-String endpoint_email = "http://www.padulab.it/nodoremoto/send_email_event.php";
-
-// Endpoint API
-String endpoint_API = "http://www.padulab.it/nodoremoto/api.php";
+RTC_DS3231 rtc;
 
 // Variabile per salvare l'indirizzo IP del nodo
 String indirizzoIP;
@@ -37,32 +21,36 @@ String dataOraCorrente;
 
 // Creiamo il client UDP per ottenere data e ora tramite server NTP
 WiFiUDP ntpUDP;
-NTPClient dataoraClient(ntpUDP, "pool.ntp.org");
+NTP ntp(ntpUDP);
 
-enum {
-init_st, measure_st, takeTime_st, send_st, goToSleep_st
+enum
+{
+  init_st,
+  measure_st,
+  takeTime_st,
+  send_st,
+  goToSleep_st
 };
 
-static uint8_t  phase=init_st;
-static uint32_t global_timer=0;
+static uint8_t phase = init_st;
+static uint32_t global_timer = 0;
+
+int32_t delta_sensor_value = 0;
+int32_t mean_sensor_value = 0;
+uint16_t n_sensor_value = 0;
 
 void sendEmailAlert();
-String getDataOraCorrente(NTPClient client);
-
-const int pwmSensorPin = 16;
-const int readSensorPin = A0;
-
-const int freq = 50000;
-const int sensorChannel = 2;
-const int resolution = 8;
-
-int32_t delta_sensor_value=0;
-int32_t mean_sensor_value=0;
-uint16_t n_sensor_value=0;
+String getDataEOra(struct tm *ptm);
+struct tm dataTimeToTM(DateTime datetime);
 
 void setup()
 {
   Serial.begin(115200);
+  EEPROM.begin(EEPROM_SIZE);
+  if (rtc.begin())
+    Serial.println("\nRTC_OK");
+  else
+    Serial.println("RTC_KO");
 
   // Ci connettiamo alla rete Wi-Fi
   Serial.print("Mi sto connettendo alla rete Wi-Fi ");
@@ -78,97 +66,105 @@ void setup()
       esp_restart();
   }
   // Connessione avvenuta con successo. Mostriamo l'indirizzo IP
-  Serial.println("");
-  Serial.println("Connesso alla rete.");
-  Serial.println("Il mio indirizzo IP è: ");
+  Serial.println("WIFI_OK");
 
   // Il metodo localIP restituisce un vettore di 4 celle. Ognuna contiene parte dell'indirizzo IP
   // Ognuna va convertita in stringa
+  //  Serial.println("Il mio indirizzo IP è: ");
+  indirizzoIP = String(WiFi.localIP()[0])+ "."  + String(WiFi.localIP()[1]) + "." + String(WiFi.localIP()[2]) + "." + String(WiFi.localIP()[3]);
+  //Serial.println(indirizzoIP);
+  if (EEPROM.read(CLOCK_SET_ADDR) != 0x0C)
+  {
+    //TIME ZONE HANDLER
+    ntp.ruleDST("CEST", Last, Sun, Mar, 2, 120); // last sunday in march 2:00, timetone +120min (+1 GMT + 1h summertime offset)
+    ntp.ruleSTD("CET", Last, Sun, Oct, 3, 60);   // last sunday in october 3:00, timezone +60min (+1 GMT)
+    ntp.begin();
+    ntp.isDST(true);
+    ntp.update();
+    rtc.disable32K();
+    rtc.adjust(DateTime(ntp.year() + 100, ntp.month(), ntp.day(), ntp.hours(), ntp.minutes(), ntp.seconds()));
+    EEPROM.write(CLOCK_SET_ADDR, 0x01);
+    while (!EEPROM.commit());
+    Serial.println("RTC setted");
 
-  indirizzoIP = String(WiFi.localIP()[0]) + "." + String(WiFi.localIP()[1]) + "." + String(WiFi.localIP()[2]) + "." + String(WiFi.localIP()[3]);
-  Serial.println(indirizzoIP);
+    DateTime Alarm_every_15min(0, 0, 0, 0, 15, 0);
+    if (rtc.setAlarm1(Alarm_every_15min, DS3231_A1_Minute))
+      Serial.println("Alarm set");
+    else
+      Serial.println("Alarm set error");
+  }
 
-  //Inizializziamo il client per ottenere data e ora
-  dataoraClient.begin();
-  // In base al nostro fuso orario, impostiamo l'offset in secondi.
-  // Noi (Italia) ci troviamo in GMT + 1 perciò sono 3600 secondi
-
-  dataoraClient.setTimeOffset(3600);
-  
-  ledcSetup(sensorChannel, freq, resolution);
-  ledcAttachPin(pwmSensorPin, sensorChannel);
-  ledcWrite(sensorChannel, 150);
+  ledcSetup(PWM_SENSOR_CHANNEL, PWM_SENSOR_FREQ, PWM_SENSOR_RESOLUTION);
+  ledcAttachPin(PWM_SENSOR_PIN, PWM_SENSOR_CHANNEL);
+  ledcWrite(PWM_SENSOR_CHANNEL, 150);
 }
 
 void loop()
 {
-  switch(phase)
+  switch (phase)
   {
-    case init_st:
-      Serial.println("fase 0");
-      mean_sensor_value=0;
-      resetTimer(&global_timer);
-      phase++;
+  case init_st:
+    Serial.println("fase 0");
+    mean_sensor_value = 0;
+    resetTimer(&global_timer);
+    phase++;
     break;
-    
-    case measure_st:
-    #define N_SAMPLES 100
+
+  case measure_st:
+#define N_SAMPLES 100
+  {
+    uint32_t time = getTime(global_timer);
+    Serial.println("fase 1: ");
+    if (time > (uint32_t)10000)
     {
-      uint32_t time=getTime(global_timer);
-      Serial.println("fase 1: ");
-      if(time>(uint32_t)10000){
-        for(int i=0;i<N_SAMPLES;i++){
-          Serial.println("N : "+ String(n_sensor_value));
-          n_sensor_value++;
-          delta_sensor_value=(int32_t)(analogRead(readSensorPin)-mean_sensor_value);
-          mean_sensor_value+=(delta_sensor_value/n_sensor_value);
-        }
-        Serial.println("value:"+String(mean_sensor_value));
-        phase++;
+      for (int i = 0; i < N_SAMPLES; i++)
+      {
+        Serial.println("N : " + String(n_sensor_value));
+        n_sensor_value++;
+        delta_sensor_value = (int32_t)(analogRead(READ_SENSOR_PIN) - mean_sensor_value);
+        mean_sensor_value += (delta_sensor_value / n_sensor_value);
       }
-    }
-    break;
-
-    case takeTime_st:
-      Serial.println("fase 2");
-      dataOraCorrente = getDataOraCorrente(dataoraClient);
-      phase++;
-    break;
-
-    case send_st:
-    {
-      Serial.println("fase 3");
-      //Prepara messaggio da POSTARE
-      String datiPOST = "valore_sensore=" + String(mean_sensor_value) + "&data_ora_creazione=" + dataOraCorrente + "&nome_nodo=" + nomeNodo + "&ip_nodo=" + indirizzoIP;
-      // Impostiamo il client HTTP per eseguire la richiesta
-      HTTPClient richiestaHTTP;
-      richiestaHTTP.begin(endpoint_API);
-      richiestaHTTP.addHeader("Content-Type", "application/x-www-form-urlencoded");
-      // Creiamo una variabile per salvare il codice della risposta (se è 200 è andata a buon fine!)
-      auto rispostaCodiceHTTP = richiestaHTTP.POST(datiPOST);
-      // Creiamo una variabile per salvare il testo della risposta: in altre parole quello che abbiamo messo come risposta nel file PHP dell'API
-      richiestaHTTP.getString();
-      // Chiudiamo la richiesta
-      richiestaHTTP.end();
+      Serial.println("value:" + String(mean_sensor_value));
       phase++;
     }
+  }
+  break;
+
+  case takeTime_st:
+    Serial.println("fase 2");
+    static struct tm ptm2 = dataTimeToTM(rtc.now());
+    dataOraCorrente = getDataEOra(&ptm2);
+    phase++;
     break;
 
-    case goToSleep_st:
-      Serial.println("fase 4");
-      esp_sleep_enable_timer_wakeup(SLEEP_TIME);
-      esp_deep_sleep_start();
-      phase++;
-    break;
-    
+  case send_st:
+  {
+    Serial.println("fase 3");
+    //Prepara messaggio da POSTARE
+    String datiPOST = "valore_sensore=" + String(mean_sensor_value) + "&data_ora_creazione=" + dataOraCorrente + "&nome_nodo=" + NOME_NODO + "&ip_nodo=" + indirizzoIP;
+    // Impostiamo il client HTTP per eseguire la richiesta
+    HTTPClient richiestaHTTP;
+    richiestaHTTP.begin(ENDPOINT_API);
+    richiestaHTTP.addHeader("Content-Type", "application/x-www-form-urlencoded");
+    // Creiamo una variabile per salvare il codice della risposta (se è 200 è andata a buon fine!)
+    auto rispostaCodiceHTTP = richiestaHTTP.POST(datiPOST);
+    // Creiamo una variabile per salvare il testo della risposta: in altre parole quello che abbiamo messo come risposta nel file PHP dell'API
+    richiestaHTTP.getString();
+    // Chiudiamo la richiesta
+    richiestaHTTP.end();
+    phase++;
+  }
+  break;
 
-    default:
+  case goToSleep_st:
+    Serial.println("fase 4");
+    rtc.clearAlarm(1);
+    phase++;
+    break;
+
+  default:
     break;
   }
-
-
-
-
 }
 
 void sendEmailAlert()
@@ -176,7 +172,7 @@ void sendEmailAlert()
   // Impostiamo il client HTTP per eseguire la richiesta
 
   HTTPClient richiestaHTTP;
-  richiestaHTTP.begin(endpoint_email + "?send_email=1");
+  richiestaHTTP.begin(String(ENDPOINT_EMAIL) + "?send_email=1");
   richiestaHTTP.addHeader("Content-Type", "application/x-www-form-urlencoded");
 
   // Creiamo una variabile per salvare il codice della risposta (se è 200 è andata a buon fine!)
@@ -191,18 +187,8 @@ void sendEmailAlert()
   richiestaHTTP.end();
 }
 
-String getDataOraCorrente(NTPClient client){
-  //Otteniamo data e ora di questo istante
-
-  client.update();
-// Struct per il tempo: serve per ottenere l'anno corrente
-  unsigned long epochTime = client.getEpochTime();
-
-  struct tm *ptm = gmtime((time_t *)&epochTime);
-
-  // Otteniamo le ore
-
-  String oreCorrenti = client.getFormattedTime();
+String getDataEOra(struct tm *ptm)
+{
 
   // Giorno
 
@@ -240,8 +226,43 @@ String getDataOraCorrente(NTPClient client){
 
   // Uniamo data e ora nel formato YYYY-MM-DD HH:MM:SS
 
-  String dataOraCorrente = dataCorrente + " " + oreCorrenti;
+  String oreCorrentiStringa = String(ptm->tm_hour);
+  String minutiCorrentiStringa = String(ptm->tm_min);
+  String secondiCorrentiStringa = String(ptm->tm_sec);
+
+  if (ptm->tm_hour < 10)
+  {
+    oreCorrentiStringa = "0" + oreCorrentiStringa;
+  }
+
+  if (ptm->tm_min < 10)
+  {
+    minutiCorrentiStringa = "0" + minutiCorrentiStringa;
+  }
+
+  if (ptm->tm_sec < 10)
+  {
+    secondiCorrentiStringa = "0" + secondiCorrentiStringa;
+  }
+
+  String orarioCorrente = oreCorrentiStringa + ":" + minutiCorrentiStringa + ":" + secondiCorrentiStringa;
+
+  String dataOraCorrente = dataCorrente + " " + orarioCorrente;
 
   return dataOraCorrente;
-  
+}
+
+struct tm dataTimeToTM(DateTime datetime)
+{
+
+  struct tm datetime_tm;
+
+  datetime_tm.tm_year = (int)datetime.year() - 2000;
+  datetime_tm.tm_mon = datetime.month() - 1;
+  datetime_tm.tm_mday = datetime.day();
+  datetime_tm.tm_hour = datetime.hour();
+  datetime_tm.tm_min = datetime.minute();
+  datetime_tm.tm_sec = datetime.second();
+
+  return datetime_tm;
 }
